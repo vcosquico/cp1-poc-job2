ef8ca09fce7bdfd7633de52d51aa23e0a2bebbdf
vcosquico <41107591+vcosquico@users.noreply.github.com>
2018-07-20 13:58:33 +0200

    Update copado_start.sh

--- copado_start.sh
+++ copado_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "Processing something for...30 seconds"
+echo "[c1p worker job] Processing something for...30 seconds"  | tee -a /tmp/copado_logs.txt
 sleep 30s
-echo "done! success"
+echo "[c1p worker job] done! success" | tee -a /tmp/copado_logs.txt
